cba5c298e6e1ee477f245aea5bd94503d305716a
yunyang xiong <yxiong43@wisc.edu>
2017-11-07 17:13:28 -0600

    common join gaze net

--- src/caffe/layers/euclidean_gaze_loss_layer.cpp
+++ src/caffe/layers/euclidean_gaze_loss_layer.cpp
@@ -47,6 +47,10 @@ void EuclideanGazeLossLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bott
     float norm_label = sqrt(label_x*label_x + label_y*label_y + label_z*label_z);
 
     float angle_value = (data_x*label_x+data_y*label_y+data_z*label_z) / (norm_data*norm_label);
+    if (angle_value > 1.0)
+      angle_value = 1.0;
+    else if (angle_value < -1.0)
+      angle_value = -1.0;
     accuracy += (acos(angle_value)*180)/3.1415926;
   }
   top[0]->mutable_cpu_data()[0] = accuracy/num;
